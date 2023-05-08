f1d6b78edca94870b4b35ca6f5945b34cf4ceb62
Minh Haong <HoangAkyc29>
2023-05-08 19:20:03 +0700

    update code crawl 2nd

---
+++ crawl_from_official.py
@@ -0,0 +1,122 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+import os
+import pandas as pd
+import requests
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+
+def Geturlmatchperseason(season) :
+    # Khởi tạo trình duyệt và mở trang web
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.premierleague.com/results")
+
+    # Chờ trang web load xong
+    driver.implicitly_wait(5)
+
+    # Đợi cho element của cookie xuất hiện và click vào nút accept
+    try:
+        accept_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//button[@id="onetrust-accept-btn-handler"]'))
+        )
+        accept_button.click()
+    except:
+        # Nếu không tìm thấy element của cookie hoặc không thể click được, bỏ qua
+        pass
+
+    time.sleep(5)
+    #------------------------------------------------------------------------------------------------
+
+    # Chờ cho dropdown xuất hiện và lấy đối tượng dropdown
+    wait = WebDriverWait(driver, 5)
+    dropdown = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-dropdown-current="comps"]')))
+
+    # Click vào dropdown để mở ra danh sách option
+    dropdown.click()
+
+    # Lấy danh sách các option
+    options = dropdown.find_element(By.XPATH,'../ul[@data-dropdown-list="comps"]')
+
+    # Tìm và click vào option mong muốn có data-option-name="2021/22"
+    for option in options.find_elements(By.TAG_NAME,'li'):
+        if option.get_attribute('data-option-name') == 'Premier League':
+            option.click()
+            break
+
+    #------------------------------------------------------------------------------------------------
+    # Chờ cho dropdown xuất hiện và lấy đối tượng dropdown
+    wait = WebDriverWait(driver, 10)
+    dropdown = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-dropdown-current="compSeasons"]')))
+
+    # Click vào dropdown để mở ra danh sách option
+    dropdown.click()
+
+    # Lấy danh sách các option
+    options = dropdown.find_element(By.XPATH,'../ul[@data-dropdown-list="compSeasons"]')
+
+    # Tìm và click vào option mong muốn có data-option-name="2021/22"
+    for option in options.find_elements(By.TAG_NAME,'li'):
+        if option.get_attribute('data-option-name') == season:
+            option.click()
+            break
+
+    time.sleep(3)
+    #------------------------------------------------------------------------------------------------
+
+    # Lặp lại việc lăn chuột xuống dưới cho đến khi không còn có thêm dữ liệu mới
+    for _ in range(5):
+        # Lăn chuột xuống dưới
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # Chờ 2 giây để cho trang web tải thêm dữ liệu
+        time.sleep(2)
+
+    # Tìm tất cả các thẻ div có class="fixture postMatch" và lấy ra tất cả data-href của chúng
+    post_match_divs = driver.find_elements(By.CSS_SELECTOR,'div.fixture.postMatch')
+    data_hrefs = [div.get_attribute('data-href') for div in post_match_divs]
+
+    # Đóng trình duyệt
+    driver.quit()
+
+    # In danh sách các data-href
+    print(data_hrefs)
+
+    return data_hrefs
+
+season_sample = "2022/23"
+season_list = []
+for year in range(2006, 2023):
+    season = str(year) + "/" + str(year+1)[2:]
+    season_list.append(season)
+
+print(season_list)
+
+for theseason in season_list:
+    url_match_data = Geturlmatchperseason(theseason)
+    new_season_string = theseason.replace("/", " to ")
+
+    # Tạo thư mục nếu chưa tồn tại
+    if not os.path.exists('url_match_data_official'):
+        os.makedirs('url_match_data_official')
+
+    # Tạo đường dẫn đầy đủ của file
+    file_path = os.path.join('url_match_data_official', new_season_string)
+
+    # ---------------------------------------------------------------------------------------
+    # Lưu danh sách ID vào file trong thư mục url_match_data
+
+    # Xóa file nếu đã tồn tại
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+    with open(file_path, 'w') as f:
+        for element in url_match_data:
+            f.write(element + '\n')
---
+++ crawl_match_detail_official.py
@@ -0,0 +1,187 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+import os
+import re
+import pandas as pd
+import requests
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+
+result_dir = 'url_match_data_official'
+
+# Lấy danh sách tên file trong thư mục 'url_match_data'
+file_list = os.listdir(result_dir)
+
+# Lọc ra danh sách các file có đuôi là .txt
+txt_files = [f for f in file_list]
+
+print(txt_files)
+
+#----------------------------------------------------------------------------------------------
+def Get_urlmatch_list(string_filename_txt):
+    # Xác định đường dẫn tới file txt
+    file_path = os.path.join('url_match_data_official', string_filename_txt)
+    # Đọc dữ liệu từ file txt và lấy phần tử cuối cùng của mỗi dòng
+    data_list = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            data_list.append("https:" + line.strip())
+
+    return data_list
+def GetCategory():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.premierleague.com/match/5937")
+    # Đợi cho element của cookie xuất hiện và click vào nút accept
+    try:
+        accept_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//button[@id="onetrust-accept-btn-handler"]'))
+        )
+        accept_button.click()
+    except:
+        # Nếu không tìm thấy element của cookie hoặc không thể click được, bỏ qua
+        pass
+
+    time.sleep(2)
+    li_element = driver.find_element(By.CSS_SELECTOR, "li[data-tab-index='2']")
+    li_element.click()
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    tbody = soup.find('tbody', {'class': 'matchCentreStatsContainer'})
+    p_children = tbody.find_all('p')
+    matchdata = []
+    for child in p_children:
+        matchdata.append(child.text)
+
+    # In kết quả
+    # print(p_children)
+    print(matchdata)
+
+    list2 = [matchdata[i] for i in range(len(matchdata)) if i % 3 == 1]
+    print(list2)
+
+    category_excel = []
+
+    for item in list2:
+        category_excel.append(item + "__Home")
+        category_excel.append(item + "__Away")
+
+    category_bouns = ['Team Home', 'Team Away', 'Score Home', 'Score Away']
+    category_excel = category_bouns + category_excel
+
+    print(category_excel)
+    driver.quit()
+    return category_excel
+
+#----------------------------------------------------------------------------------------------
+
+GetCategory()
+
+#----------------------------------------------------------------------------------------------
+
+def MatchDetailData(url):
+    # Tìm element và bấm
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get(url)
+
+    # Đợi cho element của cookie xuất hiện và click vào nút accept
+    try:
+        accept_button = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//button[@id="onetrust-accept-btn-handler"]'))
+        )
+        accept_button.click()
+    except:
+        # Nếu không tìm thấy element của cookie hoặc không thể click được, bỏ qua
+        pass
+
+    time.sleep(2)
+    li_element = driver.find_element(By.CSS_SELECTOR,"li[data-tab-index='2']")
+    li_element.click()
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    tbody = soup.find('tbody', {'class': 'matchCentreStatsContainer'})
+    p_children = tbody.find_all('p')
+    matchdata = []
+    for child in p_children:
+        matchdata.append(child.text)
+
+    # In kết quả
+    #print(p_children)
+    print(matchdata)
+
+    list1 = [matchdata[i] for i in range(len(matchdata)) if i % 3 == 0]
+    #list2 = [matchdata[i] for i in range(len(matchdata)) if i % 3 == 1]
+    list3 = [matchdata[i] for i in range(len(matchdata)) if i % 3 == 2]
+
+    print(list1)
+    #print(list2)
+    print(list3)
+
+    #-----------------------------------------------------------------------------------------
+
+    resultdiv = soup.find('div', {'class': 'score fullTime'})
+
+    score_text = resultdiv.text.strip()  # lấy nội dung của thẻ div và xóa các ký tự trắng ở đầu và cuối
+    scores = re.findall('\d+', score_text)  # sử dụng regular expression để tìm các giá trị số trong nội dung
+    print(scores)
+    list1.insert(0,scores[0])
+    list3.insert(0,scores[1])
+    #-----------------------------------------------------------------------------------------
+
+    home_team_div = soup.find('div', {'class': 'team home'})
+    home_team_long_span = home_team_div.find('span', {'class': 'long'})
+
+    home_team_name = home_team_long_span.text
+
+    print(home_team_name)
+    list1.insert(0,home_team_name)
+
+    away_team_div = soup.find('div', {'class': 'team away'})
+    away_team_long_span = away_team_div.find('span', {'class': 'long'})
+
+    away_team_name = away_team_long_span.text
+
+    print(away_team_name)
+    list3.insert(0,away_team_name)
+
+    #-----------------------------------------------------------------------------------------
+    result = []
+
+    for a, b in zip(list1, list3):
+        result.append(a)
+        result.append(b)
+
+    print(result)
+    # Đóng trình duyệt
+    driver.quit()
+
+    return result
+
+for filename in txt_files:
+    Seasondata = []
+    for url in Get_urlmatch_list(filename):
+        Seasondata.append(MatchDetailData(url))
+
+    # Tạo thư mục nếu chưa tồn tại
+    dir_name = 'final_result_data_official'
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+    # Tạo DataFrame từ list và lưu vào file excel
+    df = pd.DataFrame(Seasondata, columns=GetCategory())
+    df['Score Home'] = pd.to_numeric(df['Score Home'], errors='coerce')
+    df['Score Away'] = pd.to_numeric(df['Score Away'], errors='coerce')
+    file_name_xml = filename + '.xlsx'  # Tên file được lấy từ url ban đầu
+    file_path = os.path.join(dir_name, file_name_xml)
+    df.to_excel(file_path, index=False)
+
+
